9b091e2159c25ef23250629a7589b7aac0e51274
mrg <rgmengedoth@gmail.com>
2023-07-12 10:21:47 +0200

    HashMap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,23 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        /*
+        List<Pet> myPets = new ArrayList<>();
+        myPets.add(wuffi);
+        myPets.add(mutzi);
+        */
+
+        /*
+        Set<Pet> myPets = new HashSet<>();
+        myPets.add(wuffi);
+        myPets.add(mutzi);
+        */
+
+
+
         Pet wuffi = new Pet();
         wuffi.setName("Wuffi");
         wuffi.setType("Dog");
@@ -14,25 +26,26 @@ public class Main {
         mutzi.setName("Mutzi");
         mutzi.setType("Cat");
 
-        Set<Pet> myPets = new HashSet<>();
-        myPets.add(wuffi);
-        myPets.add(mutzi);
-
-        /*
-        List<Pet> myPets = new ArrayList<>();
-        myPets.add(wuffi);
-        myPets.add(mutzi);
-        */
+        Map<String, Pet> myPets = new HashMap<>();
+        myPets.put(wuffi.getName(), wuffi);
+        myPets.put(mutzi.getName(), mutzi);
 
 
         Pet tweety = new Pet();
         tweety.setName("Tweety");
         tweety.setType("Bird");
-        myPets.add(tweety);
+        myPets.put(tweety.getName(), tweety);
 
-        for (Pet anyPet : myPets){
+        Pet fav = myPets.get("Mutzi");
+        System.out.println("Ich mag " + fav);
+
+        for (Pet anyPet : myPets.values()){             // myPets.values() nur bei HashMap verwendung
             System.out.println("Ich füttere " + anyPet);
         }
+
+/*        for (Pet anyPet : myPets){                    // myPets verwendung bei Listen und HastSet
+            System.out.println("Ich füttere " + anyPet);
+        }*/
 
     }
 
